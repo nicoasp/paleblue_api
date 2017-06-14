e1f2206510bbc4bce6020391649a60929a34c9c1
markmarkyesyes <markhahn545@gmail.com>
2017-06-14 10:46:37 -0500

    changed login/register routes to only return user id in place of the whole user object

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -3,8 +3,7 @@ const models = require("../models");
 const User = models.User;
 
 const Content = models.Content;
-const request = require('request');
-
+const request = require("request");
 
 describe("App", () => {
   const baseUrl = "http://localhost:8888";
@@ -12,13 +11,12 @@ describe("App", () => {
   let server;
   let user;
 
-  beforeEach((done) => {
+  beforeEach(done => {
     server = app.listen(8888, () => {
       User.create({
-        email: 'foobar@gmail.com',
-        password: 'password'
-      })
-      .then((result) => {
+        email: "foobar@gmail.com",
+        password: "password"
+      }).then(result => {
         user = result;
         done();
       });
@@ -36,89 +34,133 @@ describe("App", () => {
     return `${apiUrl}${type}?token=${user.token}${params}`;
   };
 
-
-  xit('registers creates user if they dont exist', (done) => {
-    request.post({url:`${apiUrl}register`, form:{
-      email: 'foobar11@gmail.com',
-      password: 'password'
-    }}, ((err, res, body) => {
-      User.findOne({email:'foobar11@gmail.com'},(err,user) => {
-        expect(user.email).toBe('foobar11@gmail.com')
-        done()
-      })
-    }))
-  });
-
-  xit("should not allow passwords less than 8 chars", done => {
-    request.post({url:`${apiUrl}register`, form:{
-      email: 'foobar1@gmail.com',
-      password: 'pas'
-    }}, ((err, res, body) => {
-      expect(res.statusCode).toBe(400)
-      done()
-    }))
-  })
-
-  xit("logs in a user if they exist", done => {
-    request.post(
-      {
-        url: `${apiUrl}login`,
-        form: {
-          email: "foobar@gmail.com",
-          password: "password"
+  describe("authorization routes", () => {
+    it("registers creates user if they dont exist", done => {
+      request.post(
+        {
+          url: `${apiUrl}register`,
+          form: {
+            email: "foobar11@gmail.com",
+            password: "password"
+          }
+        },
+        (err, res, body) => {
+          User.findOne({ email: "foobar11@gmail.com" }, (err, user) => {
+            expect(user.email).toBe("foobar11@gmail.com");
+            done();
+          });
         }
-      },
-      (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        done();
-      }
-    );
+      );
+    });
+
+    it("should not allow passwords less than 8 chars", done => {
+      request.post(
+        {
+          url: `${apiUrl}register`,
+          form: {
+            email: "foobar1@gmail.com",
+            password: "pas"
+          }
+        },
+        (err, res, body) => {
+          expect(res.statusCode).toBe(400);
+          done();
+        }
+      );
+    });
+
+    it("logs in a user if they exist", done => {
+      request.post(
+        {
+          url: `${apiUrl}login`,
+          form: {
+            email: "foobar@gmail.com",
+            password: "password"
+          }
+        },
+        (err, res, body) => {
+          expect(res.statusCode).toBe(200);
+          done();
+        }
+      );
+    });
   });
 
   describe("Content", () => {
     let token;
-    beforeEach((done) => {
-      request.post({url:`${apiUrl}login`, form:{
-        email: 'foobar@gmail.com',
-        password: 'password'
-      }}, ((err, res, body) => {
-        token = JSON.parse(body).token;
-        done();
-      }))
+    beforeEach(done => {
+      request.post(
+        {
+          url: `${apiUrl}login`,
+          form: {
+            email: "foobar@gmail.com",
+            password: "password"
+          }
+        },
+        (err, res, body) => {
+          token = JSON.parse(body).token;
+          done();
+        }
+      );
     });
 
-
-
-    it('creates new image content', (done) => {
+    it("creates new image content", done => {
       var options = {
         url: `${apiUrl}content`,
         method: "POST",
         headers: {
-          'Authorization': `JWT ${token}`
+          Authorization: `JWT ${token}`
         },
         form: {
-          contentType: 'image',
-          data: "https://i0.wp.com/st.gdefon.ru/wallpapers_original/wallpapers/393789_tigry_art_planeta_zemlya_1680x1050_(www.GdeFon.ru).jpg",
+          contentType: "image",
+          data:
+            "https://i0.wp.com/st.gdefon.ru/wallpapers_original/wallpapers/393789_tigry_art_planeta_zemlya_1680x1050_(www.GdeFon.ru).jpg",
           lng: "-71.2760",
-          lat: "42.4906",
+          lat: "42.4906"
         }
       };
-      request(options, ((err, res, body) => {
-        console.log("create content log", body);
-        Content.findOne({})
-        .then((content) => {
-          if (err) {console.log("error is...", err)}
-          console.log("content", content)
-          expect(content.lng).toBe(-71.2760)
-          done()
-        })
-
-      }))
+      request(options, (err, res, body) => {
+        Content.findOne({}).then(content => {
+          if (err) {
+            console.log("error is...", err);
+          }
+          expect(content.lng).toBe(-71.276);
+          done();
+        });
+      });
     });
-  })
+  });
 
-
-
-
+  describe("error handling", () => {
+    let token;
+    beforeEach(done => {
+      request.post(
+        {
+          url: `${apiUrl}register`,
+          form: {
+            email: "foobar11@gmail.com",
+            password: "password"
+          }
+        },
+        (err, res, body) => {
+          token = JSON.parse(body).token;
+          done();
+        }
+      );
+    });
+    it("returns an error when incorrect login info is provided", done => {
+      request.post(
+        {
+          url: `${apiUrl}login`,
+          form: {
+            email: "foobar99@gmail.com",
+            password: "password"
+          }
+        },
+        (err, res, body) => {
+          console.log(err);
+        }
+      );
+    });
+  });
 });
-
