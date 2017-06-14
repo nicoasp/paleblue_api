58580f603fcf6750d0491afadab4e45360526fd4
markmarkyesyes <markhahn545@gmail.com>
2017-06-14 11:27:38 -0500

    fixed error handling in content routes

--- routers/content.js
+++ routers/content.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const mongoose = require('mongoose');
-const models = require('../models');
+const mongoose = require("mongoose");
+const models = require("../models");
 const Content = models.Content;
 
-router.get('/', (req, res) => {
-   Content.find({})
-	   .then((contentList) => {
-	   	res.json(contentList);
-	   })
-	   .catch((err) => {
-	   	console.log(error);
-	   })
-})
+router.get("/", (req, res) => {
+  Content.find({})
+    .then(contentList => {
+      res.json(contentList);
+    })
+    .catch(err => {
+      console.log(err);
+      next({ status: 404, error: "Content does not exist" });
+    });
+});
 
-router.post('/', (req, res) => {
-	const { contentType, data, lng, lat } = req.body;
+router.post("/", (req, res) => {
+  const { contentType, data, lng, lat } = req.body;
 
   const content = new Content();
   content.contentType = contentType;
@@ -24,18 +25,15 @@ router.post('/', (req, res) => {
   content.lng = lng;
   content.lat = lat;
   content.save((err, content) => {
-  	if (err) {
-  		res.json({
-  			success: false,
-  			error: err
-  		})
-  	} else {
-  		res.json({
-  			success: true,
-  			error: null
-  		})  		
-  	}
-  })
-})
+    if (err) {
+      next({ status: 400, error: "Submitted content is not valid" });
+    } else {
+      res.json({
+        success: true,
+        error: null
+      });
+    }
+  });
+});
 
 module.exports = router;
