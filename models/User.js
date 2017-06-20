d5ced0d3c24eff68a7f2cdd8f26fbc92a1085bd4
markmarkyesyes <markhahn545@gmail.com>
2017-06-20 11:45:48 -0500

    added verified field to User model, defaults false

--- models/User.js
+++ models/User.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
-const mongooseuniquevalidator = require('mongoose-unique-validator');
-const bcrypt = require('bcrypt-nodejs');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const mongooseuniquevalidator = require("mongoose-unique-validator");
+const bcrypt = require("bcrypt-nodejs");
 
 let UserSchema = new Schema({
-  email: {type: String, isEmail: true, unique: true},
-  passwordHash: {type: String},
-  lastActive: { type: Date, default: Date.now }
+  email: { type: String, isEmail: true, unique: true },
+  passwordHash: { type: String },
+  lastActive: { type: Date, default: Date.now },
+  verified: { type: Boolean, default: false }
 });
 
 UserSchema.methods.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.passwordHash);
 };
 
-UserSchema.virtual('password').set(function(value) {
-    this.passwordHash = bcrypt.hashSync(value);
+UserSchema.virtual("password").set(function(value) {
+  this.passwordHash = bcrypt.hashSync(value);
 });
 
 UserSchema.plugin(mongooseuniquevalidator);
 
-var User = mongoose.model('User', UserSchema);
+var User = mongoose.model("User", UserSchema);
 
-module.exports = User
+module.exports = User;
