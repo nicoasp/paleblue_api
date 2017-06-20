fa4f33246c665f40ffb055f2888a04e7a9379e1b
nicoasp <nicolas.amaya@gmail.com>
2017-06-20 18:34:29 +0200

    change data creation to faker

--- demoScript.js
+++ demoScript.js
@@ -2,11 +2,7 @@ const models = require("./models");
 const User = models.User;
 const Content = models.Content;
 const Like = models.Like;
-
-// Start the script by asking the user to enter some text content.
-// Store this contentId for later use
-const demoUserId = "demoUserId";
-const demoContentId = "demoContentId";
+const faker = require('faker');
 
 
 // All the content and likes can be done by one single fake user, since we don't make any conditions on like ids or locations for user contents.
@@ -21,41 +17,87 @@ User.create({
 })
 
 
-const imageUrls = ["url1", "url2"]
-const textMessages = ["text1", "text2"]
-const coordinates = [{lng: 1, lat: 1}, {lng: 2, lat: 2}] 
 
-// Create a bunch of content programmatically with those 3 things.
-let contentArray = [];
-
-imageUrls.forEach((url) => {
-	let { lng, lat} = coordinates[Math.floor(Math.random() * coordinates.length)]
-  Content.create({
-    userId: user._id,
-    contentType: "image",
-    data: url,
-    lng,
-    lat
-  })
-  .then((savedContent) => {
-    content = savedContent;
-    done();        
-  })
-})
-
-
-
-
-
-
-const sayHello = () => {
+const createAndSendContent = (socket, demoUserId, delay, postedContent) => {
 	return setTimeout(() => {
-		console.log("Hello");
-	}, 3000)
+    let contentType, data;
+    if (Math.floor(Math.random() * 2)) {
+      contentType = "image";
+      data = faker.Image.imageUrl();
+    } else {
+      contentType = "text";
+      data = faker.Lorem.paragraph();
+    }
+    Content.create({
+      userId: user._id,
+      contentType,
+      data,
+      lng: faker.Address.longitude(),
+      lat: faker.Address.latitude(),
+      demoId: demoUserId
+    })
+    .then((savedContent) => {
+      postedContent.push(content);
+      socket.emit("new content", content);           
+    })    
+	}, delay)
 }
 
-// setTimeout(() => {
-// 	console.log("Hello");
-// }, 3000)
+const sendLike = (socket, demoUserId, delay, postedContent) => {
+  return setTimeout(() => {
+    if (postedContent.length) {
+      let { fromLng, fromLat} = coordinates[Math.floor(Math.random() * coordinates.length)]
+      Like.create({
+        fromUserId: user._id,
+        contentId: postedContent[Math.floor(Math.random() * postedContent.length)]
+        fromLng,
+        fromLat,
+        demoId: demoUserId
+      })
+      .then((savedLike) => {
+        socket.emit("new like", savedLike);    
+      })
+    }    
+  }, delay)
+}
 
-sayHello();
+
+const mainScript = (socket, demoUserId, demoContentId, text) => {
+
+  // Give the already created user it's own demoId
+  User.findById(demoUserId)
+  .then(demoUser => {
+    if (demoUser) {
+      demoUser.demoId = demoUserId;
+      demoUser.save((err, updatedUser) => {
+        if (err) { console.log(err); }
+      })        
+    }
+  }) 
+  // Give the content he created the demoId
+  Content.findById(demoContentId)
+  .then(demoContent => {
+    if (demoContent) {
+      demoContent.demoId = demoUserId;
+      demoContent.save((err, updatedContent) => {
+        if (err) { console.log(err); }
+      })        
+    }
+  }) 
+
+  let postedContent = [];
+  // For each created piece of content, publish it at a random time
+  while (contentArray.length) {
+    let content = contentArray.pop();
+    let delay = Math.floor(Math.random() * 300 + 1) * 1000;
+    sendContent(socket, content, delay, postedContent);
+  }
+  for (let i = 0; i < 20; i++) {
+
+  }
+}
+
+
+
+
+
